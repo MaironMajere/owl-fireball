44f07ea271f74b91e5068db9bbc6cc9886a4dd24
MaironMajere <sergiolamir2003@gmail.com>
2026-06-03 22:16:02 +0200

    Create background.js
    хуй

---
+++ background.js
@@ -0,0 +1,11 @@
+OBR.onReady(() => {
+    OBR.tool.create({
+        id: "com.fireball.calculator/tool",
+        icons: [
+            {
+                icon: "https://maironmajere.github.io/owl-fireball/icon.svg",
+                label: "AD&D Fireball 3D",
+            },
+        ],
+    });
+});
